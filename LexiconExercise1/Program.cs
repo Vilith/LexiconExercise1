b7b40132e0748188776fedc60e551150e0a254d3
Vilith <vilith44@gmail.com>
2025-04-01 14:00:03 +0200

    Add project files.

---
+++ LexiconExercise1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+1. Registret skall kunna ta emot och lagra anställda med namn och lön. (Via inmatning i konsolen, inget krav på persistent lagring).
+2. Programmet skall kunna skriva ut registret i en konsol.
+
+
+Uppgift 1 - Vilka klasser bör ingå i programmet?
+Uppgift 2 - Vilka attribut och metoder bör ingå i dessa klasser?
+Uppgift 3 - Skriva programmet.
+*/
+
+namespace LexiconExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
